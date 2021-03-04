a227a93527cc0dcbad8f4c2891d97b43ad350296
seysdsv <desarrollo@seys.es>
2021-03-04 13:08:19 +0100

    Update env.js

--- env.js
+++ env.js
@@ -104,7 +104,7 @@ var
 
   ////////uncomment the following line when downloading the app
 
-  //apiUrl = 'https://js.arcgis.com/3.31';
+  apiUrl = 'https://js.arcgis.com/3.31';
 
   //////////////////////////////////////////////////////////////
   allCookies = getAllCookies();
